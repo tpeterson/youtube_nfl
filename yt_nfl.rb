97a63b5e32956c40ea3da9d995efa9cb6aa7d229
Tim <tpeterson@users.noreply.github.com>
2015-01-27 19:43:22 -0800

    Add Ruby script that saves to JSON
    Ruby script grabs the headline, url and views for each of the 30 most
    recently uploaded videos listed on the NFL’s YouTube channel.
    Information is grouped together as a hash for each video saved as JSON
    in a txt file.

---
+++ yt_nfl.rb
@@ -0,0 +1,42 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'csv'
+require 'json'
+
+URL_FILE = "nfl_ytviews.txt"
+ytvids = []
+
+url = 'https://www.youtube.com/channel/UCDVYQ4Zhbm3S2dlz7P1GBDg/videos'
+
+page = Nokogiri::HTML(open(url))
+
+page.css('ul#browse-items-primary li .yt-lockup-content').each do |video|
+  headline = video.css('.yt-lockup-title a')
+  title = headline.inner_html
+  url = video.at_css('.yt-lockup-title')
+  get_link = url.xpath('a/@href')
+  link = "http://www.youtube.com" + get_link.to_s
+  viewcount = video.css('.yt-lockup-meta .yt-lockup-meta-info li:nth-child(1)')
+  views = viewcount.inner_html
+  uploaded = video.css('.yt-lockup-meta .yt-lockup-meta-info li:nth-child(2)')
+  upload_date = uploaded.inner_html
+  date = Time.now
+
+  ytvids.push(
+    title: title,
+    views: views,
+    link: link,
+    upload_date: upload_date,
+    date: date
+  )
+end
+
+File.open(URL_FILE, 'w') do |f|
+  f.puts ytvids
+end
+
+puts "Copied #{ytvids.length} titles"
+puts ytvids[0][:title]
+puts ytvids[0][:link]
+puts ytvids[0][:views]
